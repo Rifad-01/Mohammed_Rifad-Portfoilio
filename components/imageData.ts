9a312da9da9fa7016ba3a2143e352f2dc2fbddf3
Mohammed Rifad NV <141805146+Rifad-01@users.noreply.github.com>
2025-08-08 11:24:58 +0530

    Fix image path for deployment

--- components/imageData.ts
+++ components/imageData.ts
@@ -1,2 +1,2 @@
 // This file stores the Base64 encoded image data to keep the component files cleaner.
-export const profileImageAsBase64 = "/public/rifaddoc-removebg-preview (1).png";
+export const profileImageAsBase64 = "/rifaddoc-removebg-preview (1).png";
